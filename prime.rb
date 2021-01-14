538940e65c94ec1df33a3dd6d0ed3cab73a57772
Eva <yi.zheng1709@gmail.com>
2021-01-13 21:01:51 -0500

    commit tree f02be94360c6645fa453e7e135fc7f1045fd8004

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(input)
+    if input < 2 #accounting for negatives numbers & 1 & 0
+        false 
+    elsif input > 2 
+        n = 2
+        while n < input # loop only until n < input cuz input is always divisible by itself
+            return false if input % n == 0 # if the input is divisible by anything else prior to itself, then it's not prime
+            n += 1
+        end 
+        true
+    elsif input == 2 #special case of 2
+        true 
+    end 
+end  
